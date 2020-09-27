260c420088bca442c6af4ce38391e3d35766c993
Jishant Singh <jishant.singh@bounceshare.com>
2020-09-27 09:14:55 +0530

    processes practice

---
+++ processes/c_shared_consumer.c
@@ -0,0 +1,29 @@
+//
+// Created by Jishant Singh on 27/09/20.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+int main() {
+/* the size (in bytes) of shared memory object */
+    const int SIZE = 4096;
+/* name of the shared memory object */
+    const char *name = "OS";
+/* shared memory file descriptor */
+    int fd;
+/* pointer to shared memory obect */
+    char *ptr;
+/* open the shared memory object */
+    fd = shm_open(name, O_RDONLY, 0666);
+/* memory map the shared memory object */
+    ptr = (char *)
+            mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+/* read from the shared memory object */
+    printf("%s", (char *) ptr);
+    shm_unlink(name);
+    return 0;
+}
