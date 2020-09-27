66c6c70821b74fff0db20f5cbc8d6f05b91ea772
Jishant Singh <jishantiitkanpur@gmail.com>
2020-09-27 09:45:00 +0530

    resolved shared memory producer issue for mac and linux

--- processes/shared_memory_producer.c
+++ processes/shared_memory_producer.c
@@ -9,6 +9,7 @@
 #include <sys/stat.h>
 #include <sys/mman.h>
 #include <unistd.h>
+#include <sys/errno.h>
 //#include <zconf.h>
 
 int main()
@@ -19,11 +20,12 @@ int main()
     const char *message_1 = "World!";
     int fd;
     char *ptr;
-    fd = shm_open(name,O_CREAT | O_RDWR,0666);
+    fd = shm_open(name,O_CREAT | O_RDWR,0777);
     printf("%d\n", fd);
     ftruncate(fd, SIZE);
     ptr = (char *)
             mmap(NULL, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    printf("%p\n", ptr);
     char *ptr2 = ptr;
     sprintf(ptr,"%s",message_0);
     ptr += strlen(message_0);
@@ -33,10 +35,13 @@ int main()
 //    shm_unlink(name);
 //    sleep(60);
 
-    fd = shm_open(name, O_RDONLY, 0666);
+    fd = shm_open(name, O_RDWR, 0777);
     printf("%d\n",fd);
     char *ptry = (char *)
             mmap(NULL, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    printf("%d\n",errno);
+    printf("%p\n", ptry);
     printf("%s\n", ptry);
+
     return 0;
 }
--- rough.c
+++ rough.c
@@ -24,7 +24,9 @@ int main(void)
     // Write initial contents to shared memory.
     write(fd1, buf, 4096);
     p1 = mmap(NULL, 4096, PROT_READ|PROT_WRITE, MAP_SHARED, fd1, 0);
+    printf("%d\n",errno);
     p2 = mmap(NULL, 4096, PROT_READ|PROT_WRITE, MAP_SHARED, fd2, 0);
+    printf("%d\n",errno);
     printf("fd1 = %d, p1 = %p\n", fd1, p1);
     printf("fd2 = %d, p2 = %p\n", fd2, p2);
     printf("p1[0] = 0x%08x, p2[0] = 0x%08x\n", p1[0], p2[0]);
