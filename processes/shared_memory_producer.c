c1570846fde30c7aa4568a13cf827411f160f81b
Jishant Singh <jishantiitkanpur@gmail.com>
2020-09-27 09:14:55 +0530

    processes practice

---
+++ processes/shared_memory_producer.c
@@ -0,0 +1,42 @@
+//
+// Created by Jishant Singh on 27/09/20.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
+//#include <zconf.h>
+
+int main()
+{
+    const int SIZE = 4096;
+    const char *name = "OS";
+    const char *message_0 = "Hello";
+    const char *message_1 = "World!";
+    int fd;
+    char *ptr;
+    fd = shm_open(name,O_CREAT | O_RDWR,0666);
+    printf("%d\n", fd);
+    ftruncate(fd, SIZE);
+    ptr = (char *)
+            mmap(NULL, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    char *ptr2 = ptr;
+    sprintf(ptr,"%s",message_0);
+    ptr += strlen(message_0);
+    sprintf(ptr,"%s",message_1);
+    ptr += strlen(message_1);
+    printf("%s\n",ptr2);
+//    shm_unlink(name);
+//    sleep(60);
+
+    fd = shm_open(name, O_RDONLY, 0666);
+    printf("%d\n",fd);
+    char *ptry = (char *)
+            mmap(NULL, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    printf("%s\n", ptry);
+    return 0;
+}
--- rough.c
+++ rough.c
@@ -4,6 +4,34 @@
 
 #include <sched.h>
 
-int main(int argc, char **argv){
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
 
+#define TEMP_FILENAME   "shm.tmp"
+
+int main(void)
+{
+    int fd1 = shm_open(TEMP_FILENAME, O_CREAT | O_RDWR, 0777);
+    int fd2 = shm_open(TEMP_FILENAME, O_RDWR, 0777);
+    int *p1, *p2;
+    int buf[1024] = {0x12345678};
+
+    // Write initial contents to shared memory.
+    write(fd1, buf, 4096);
+    p1 = mmap(NULL, 4096, PROT_READ|PROT_WRITE, MAP_SHARED, fd1, 0);
+    p2 = mmap(NULL, 4096, PROT_READ|PROT_WRITE, MAP_SHARED, fd2, 0);
+    printf("fd1 = %d, p1 = %p\n", fd1, p1);
+    printf("fd2 = %d, p2 = %p\n", fd2, p2);
+    printf("p1[0] = 0x%08x, p2[0] = 0x%08x\n", p1[0], p2[0]);
+    p1[0] = 0xdeadbeef;
+    printf("p1[0] = 0x%08x, p2[0] = 0x%08x\n", p1[0], p2[0]);
+    close(fd2);
+    close(fd1);
+    shm_unlink(TEMP_FILENAME);
+    return 0;
 }
