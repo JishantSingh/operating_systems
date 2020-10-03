22e56764f70856cfd148e3c061487ea1c840d07b
Jishant Singh <jishantiitkanpur@gmail.com>
2020-10-04 05:01:01 +0530

    time measurement wrapper

--- processes/exec_test.c
+++ processes/exec_test.c
@@ -2,7 +2,12 @@
 // Created by Jishant Singh on 03/10/20.
 //
 #include <unistd.h>
+#include <sys/time.h>
+#include "stdio.h"
 int main(){
-    execl("/usr/bin/ls","ls",NULL);
+//    execlp("ls","ls",NULL);
+    struct timeval cur;
+    gettimeofday(&cur,NULL);
+    printf("%ld", (long)cur.tv_sec);
     return 0;
 }
