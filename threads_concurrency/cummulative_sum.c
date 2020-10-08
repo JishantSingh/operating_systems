3c605e3da68742c02fc15761d795b0f490f55850
Jishant Singh <jishantiitkanpur@gmail.com>
2020-10-08 06:10:34 +0530

    hello world of threading

---
+++ threads_concurrency/cummulative_sum.c
@@ -0,0 +1,14 @@
+//
+// Created by Jishant Singh on 08/10/20.
+//
+
+#include <pthread.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    pthread_t tid;
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_create(&tid,&attr,)
+}
+
