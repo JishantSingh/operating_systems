26a5b6fdac0e4a5e0b7de8f5ff125aa906df7e94
Jishant Singh <jishantiitkanpur@gmail.com>
2020-10-11 19:21:27 +0530

    configures remote gcc/g++, multithreading examples using thread and openmp

--- threads_concurrency/cummulative_sum.c
+++ threads_concurrency/cummulative_sum.c
@@ -4,11 +4,24 @@
 
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+int sum = 0;
+void *f(void *p);
 int main(int argc, char *argv[]){
     pthread_t tid;
     pthread_attr_t attr;
     pthread_attr_init(&attr);
-    pthread_create(&tid,&attr,)
+    pthread_create(&tid,&attr,f,argv[1]);
+    pthread_join(tid,NULL);
+    printf("sum is %d\n",sum);
+}
+
+void* f(void* p){
+    int n = atoi(p);
+    for(int i=0;i<=n;i++){
+        sum+=i;
+    }
+    pthread_exit(0);
 }
 
